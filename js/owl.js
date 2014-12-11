ae7b6b293374cd9d6f1222cb111cb3ebb6f709c7
Baburao <dhirajsapkal@gmail.com>
2014-12-11 16:30:56 +0530

    Added owl carousel

---
+++ js/owl.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    var options={}
+    if(document.location.href.substr("accumen"))
+      options = {
+        autoPlay : 3000,
+        stopOnHover : true,
+        navigation:true,
+        paginationSpeed : 1000,
+        goToFirstSpeed : 2000,
+        singleItem : true,
+        autoHeight : true,
+        transitionStyle:"fade"
+      }
+    else
+      options = {
+        paginationSpeed : 1000,
+        goToFirstSpeed : 2000,
+        singleItem : true,
+        autoHeight : true,
+        transitionStyle:"fade"
+      }
+
+
+
+  $("#owl-demo").owlCarousel(options);
+  $("#temp1").click(function(){
+    $(".owl-wrapper").css("left", -$(".owl-item").width());
+  })
+
+  $("#temp2").click(function(){
+    $(".owl-wrapper").css("left", 0);
+  })
+
+});
---
+++ js/owl2.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+      items : 4,
+      itemsDesktop : [3000,2],
+      itemsDesktopSmall : [979,3]
+
+  });
+
+});
