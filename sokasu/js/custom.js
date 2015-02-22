441e5d75c4509490910e4a44f3f753c5422c3bb5
Baburao <dhirajsapkal@gmail.com>
2015-02-22 20:23:06 +0530

    moved wow to preloader

--- sokasu/js/custom.js
+++ sokasu/js/custom.js
@@ -1,3 +1,12 @@
+// Wait for window load
+$(window).load(function() {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");
+  console.log( "preloader" );
+  new WOW().init();
+  console.log( "started" );
+});
+
 $(document).ready(function() {
   $('#scene').parallax();
   $(window).on("resize", function(){
@@ -14,5 +23,6 @@ $(document).ready(function() {
 
 
   $('.banner').localScroll({duration:500});
+
 
 });
