194009b43a5ade6aee91b4f8b4a232f995e7a3d5
Baburao <dhirajsapkal@gmail.com>
2015-03-13 08:34:42 +0530

    added owl carousel 2

--- sokasu/js/custom.js
+++ sokasu/js/custom.js
@@ -22,25 +22,28 @@ $(document).ready(function() {
 
   $('.banner').localScroll({duration:500});
 
-  $("#owl-demo").owlCarousel({
- 
+  $(".owl-carousel").owlCarousel({
+
       navigation : false, // Show next and prev buttons
-      slideSpeed : 300,
+      slideSpeed : 500,
+      items: 1,
       paginationSpeed : 400,
+      autoplay:true,
+      autoplayTimeout:7000,
       singleItem:true,
-      autoWidth:true,
+      autoWidth:false,
       loop: true,
-      pagination: false,
-      lazyLoad: true
+      lazyLoad: true,
+      fluidSpeed: true
       // margin: 20
- 
+
       // "singleItem:true" is a shortcut for:
-      // items : 1, 
+      // items : 1,
       // itemsDesktop : false,
       // itemsDesktopSmall : false,
       // itemsTablet: false,
       // itemsMobile : false
- 
+
   });
 
 
