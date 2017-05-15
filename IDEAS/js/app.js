f5ac256a2ab52ec12d16d10651de166b890bfaa5
Dhiraj Sapkal <dhirajsapkal@gmail.com>
2017-05-15 10:21:55 -0400

    Added IDEAS folder

---
+++ IDEAS/js/app.js
@@ -0,0 +1,162 @@
+
+$(document).ready(function(){
+	$(document).foundation();
+
+	// Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+
+	//Move About
+	var  aboutText = new ScrollMagic.Scene({
+		triggerElement: '.about',
+		triggerHook: 0.7,
+		duration: '130%'
+	})
+
+
+
+	.setTween(TweenMax.from('.moveAbout', .6, {y: '100px', ease:Power0.easeNone}))
+
+
+	// .addIndicators({
+	// 	name: 'fade',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+	//Move Programs
+	var movePrograms = new ScrollMagic.Scene({
+		triggerElement: '.programs',
+		triggerHook: 0.7,
+		duration: '130%'
+	})
+
+	.setTween(TweenMax.from('.movePrograms', .6, {y: '100px', ease:Power0.easeNone}))
+
+	// .addIndicators({
+	// 	name: 'fade',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+	//Move Work
+	var movePrograms = new ScrollMagic.Scene({
+		triggerElement: '.work',
+		triggerHook: 0.7,
+		duration: '130%'
+	})
+
+	.setTween(TweenMax.from('.moveWork', .6, {y: '100px', ease:Power0.easeNone}))
+
+	// .addIndicators({
+	// 	name: 'fade',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+
+
+
+
+	//about image scene
+	var about_image_parallax = new ScrollMagic.Scene({
+		triggerElement: '.section',
+		triggerHook: 1,
+		duration: '200%'
+	})
+
+	.setTween(TweenMax.from('.about-background', .4, {y: '-40%', ease:Power0.easeNone}))
+
+	.addTo(controller);
+
+
+
+
+
+	//header parallax scene
+	var slideParallaxScene = new ScrollMagic.Scene({
+		triggerElement: '.background-parallax',
+		triggerHook: 0,
+		duration: '99%'
+	})
+
+	.setTween(TweenMax.from('.header-background', .4, {y: '-20%', ease:Power0.easeNone}))
+
+
+	// .addIndicators({
+	// 	name: 'Background-parallax',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+
+
+
+
+	//Move spring program cards
+
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '.spring',
+		triggerHook: 0.7
+	})
+	.setClassToggle('.spring', 'fade-in') // add class to project01
+
+	// .addIndicators({
+	// 	name: 'Background-parallax',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+		//Move fall program cards
+
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '.fall',
+		triggerHook: 0.8
+	})
+	.setClassToggle('.fall', 'fade-in') // add class to project01
+
+	// .addIndicators({
+	// 	name: 'Background-parallax',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+
+	//Move work projects
+
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '.projects',
+		triggerHook: 0.8
+	})
+	.setClassToggle('.projects', 'fade-in') // add class to project01
+
+	// .addIndicators({
+	// 	name: 'Background-parallax',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+})
+
+
+
