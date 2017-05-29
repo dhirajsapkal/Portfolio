a5ff4c03f240a6e67e2b3f158a6da29332b7a58f
Dhiraj Sapkal <dhirajsapkal@gmail.com>
2017-05-29 18:01:57 -0400

    Added projects page to IDEAS

--- IDEAS/js/app.js
+++ IDEAS/js/app.js
@@ -66,6 +66,9 @@ $(document).ready(function(){
 
 
 
+
+
+
 	//about image scene
 	var about_image_parallax = new ScrollMagic.Scene({
 		triggerElement: '.section',
@@ -74,6 +77,18 @@ $(document).ready(function(){
 	})
 
 	.setTween(TweenMax.from('.about-background', .4, {y: '-40%', ease:Power0.easeNone}))
+
+	.addTo(controller);
+
+
+	//project image scene
+	var about_image_parallax = new ScrollMagic.Scene({
+		triggerElement: '.section',
+		triggerHook: 1,
+		duration: '200%'
+	})
+
+	.setTween(TweenMax.from('.project-background', .4, {y: '-28%', ease:Power0.easeNone}))
 
 	.addTo(controller);
 
@@ -108,7 +123,7 @@ $(document).ready(function(){
 
 	var ourScene = new ScrollMagic.Scene({
 		triggerElement: '.spring',
-		triggerHook: 0.7
+		triggerHook: 0.9
 	})
 	.setClassToggle('.spring', 'fade-in') // add class to project01
 
@@ -125,7 +140,7 @@ $(document).ready(function(){
 
 	var ourScene = new ScrollMagic.Scene({
 		triggerElement: '.fall',
-		triggerHook: 0.8
+		triggerHook: 0.9
 	})
 	.setClassToggle('.fall', 'fade-in') // add class to project01
 
@@ -146,6 +161,57 @@ $(document).ready(function(){
 		triggerHook: 0.8
 	})
 	.setClassToggle('.projects', 'fade-in') // add class to project01
+
+	// .addIndicators({
+	// 	name: 'Background-parallax',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+	//move full image 1
+		//Move footer
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '.full-image-1',
+		triggerHook: 0.9
+	})
+	.setClassToggle('.full-image-1', 'fade-in') // add class to project01
+
+	// .addIndicators({
+	// 	name: 'Background-parallax',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+	//move full image 2
+		//Move footer
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '.full-image-2',
+		triggerHook: 0.9
+	})
+	.setClassToggle('.full-image-2', 'fade-in') // add class to project01
+
+	// .addIndicators({
+	// 	name: 'Background-parallax',
+	// 	colorTrigger: 'black',
+	// 	colorStart: '#75C695',
+	// 	colorEnd: 'pink'
+	// }) 
+
+	.addTo(controller);
+
+
+	//Move footer
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '.footer',
+		triggerHook: 0.9
+	})
+	.setClassToggle('.footer', 'fade-in') // add class to project01
 
 	// .addIndicators({
 	// 	name: 'Background-parallax',
